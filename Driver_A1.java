6ebc4090d25bdbb5f9f4583ff30531a333dd3b35
xnkasy <xnkasypriyanshu@gmail.com>
2020-12-16 10:37:55 +0530

    Added Final Drivers

---
+++ Driver_A1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Driver{
+    static public String printList(Dictionary myList){
+        StringBuilder sb = new StringBuilder();
+        while(myList!=null){
+            String temp = myList.key+"_"+myList.address+"_"+myList.size;
+            sb.append(temp);
+            myList = myList.getNext();
+        }
+        return sb.toString();
+    }
+    public static void main(String args[]){
+        int numTestCases;
+        Scanner sc = new Scanner(System.in);
+        numTestCases = sc.nextInt();
+        while(numTestCases-->0){
+            int size;
+            size = sc.nextInt();
+            A2DynamicMem obj = new A2DynamicMem(size);
+            int numCommands = sc.nextInt();
+            while(numCommands-->0) {
+                String command;
+                command = sc.next();
+                int argument;
+                argument = sc.nextInt();
+                int result = -5;
+                switch (command) {
+                    case "Allocate":
+                        result = obj.Allocate(argument);
+                        break;
+                    case "Free":
+                        result = obj.Free(argument);
+                        break;
+                    case "Defragment":
+                        obj.Defragment();
+                        break;
+                    default:
+                        break;
+                }
+                System.out.println(result);
+                System.out.println(printList(obj.freeBlk));
+                System.out.println(printList(obj.allocBlk));
+            }
+            
+        }
+    }
+}
